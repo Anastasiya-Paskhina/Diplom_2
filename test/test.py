a91400c80de319f87798bf76c57637c0514f2ca0
Anastasiya-Paskhina <45151607+Anastasiya-Paskhina@users.noreply.github.com>
2019-04-02 23:27:28 +0300

    Update test.py

--- test/test.py
+++ test/test.py
@@ -10,6 +10,15 @@ class MyTest(unittest.TestCase):
         main_user.groups()
         main_user.friends()
         self.assertIsInstance(top10(main_user)[0], dict)
+    
+        def structure_top10(self):
+        conf_structure = {
+            '_id': int,
+            'photos': str,
+            'user_id': int,
+            'user_page': str
+        }
+        self.assertDictEqual(conf_structure, top10.select_users_dict)
 
 
 if __name__ == '__main__':
